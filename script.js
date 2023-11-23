4d87742524a46390a669085af8931d9abb322a11
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-24 00:14:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
     //Write your code here
-  
+	const radius=document.getElementById("radius").value;
+	// console.log(radius);
+	let vol=4/3*Math.PI*(radius**3);
+	console.log(vol);
+	document.getElementById("volume").value=`${vol}`;
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
